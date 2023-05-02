035b29eebb22838538a775016cf3d73ab6bce9b0
boushphong <59940078+boushphong@users.noreply.github.com>
2023-05-02 21:29:19 +0700

    Update binary_search.py

--- 2. Algorithms/1. Searching/binary_search.py
+++ 2. Algorithms/1. Searching/binary_search.py
@@ -1,5 +1,5 @@
-array = [1,3,5,7,9,11,40,41]
-array1 = [1,3,5,7,9,11,40,41,44]
+array = [11,13,15,17,29,31,40,41]
+array1 = [11,13,15,17,29,31,40,41,55]
     
     
 def binary_search(arr, target):
@@ -17,3 +17,72 @@ def binary_search(arr, target):
             right = mid - 1
 
     return -1
+
+
+def binary_search_in_details(arr, target):
+    left = 0
+    right = len(arr) - 1
+    
+    print("Array Length:", len(arr))
+    print(f"First Value of index {left}:", arr[left])
+    print(f"Last Value of index {right}:", arr[right])
+    print("---")
+    
+    while left <= right:
+        mid = (left + right) // 2
+        print(f"Mid Value of index {mid}:", arr[mid])
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+            print(f"Mid value smaller than {target}, checking from left index of {left} to {right}")
+            print(f"Checking Mid index of {left} + {right} = {(left + right) // 2}")
+            print("---")
+        else:
+            right = mid - 1
+            print(f"Mid value larger than {target}, checking from left index of {left} to {right}")
+            print(f"Checking Mid index of {left} + {right} = {(left + right) // 2}")
+            print("---")
+
+    return -1
+
+
+binary_search_in_details(array1, 17)
+"""
+Array Length: 9
+First Value of index 0: 11
+Last Value of index 8: 55
+---
+Mid Value of index 4: 29
+Mid value larger than 17, checking from left index of 0 to 3
+Checking Mid index of 0 + 3 = 1
+---
+Mid Value of index 1: 13
+Mid value smaller than 17, checking from left index of 2 to 3
+Checking Mid index of 2 + 3 = 2
+---
+Mid Value of index 2: 15
+Mid value smaller than 17, checking from left index of 3 to 3
+Checking Mid index of 3 + 3 = 3
+---
+Mid Value of index 3: 17
+"""
+
+binary_search_in_details(array1, 15)
+"""
+Array Length: 9
+First Value of index 0: 11
+Last Value of index 8: 55
+---
+Mid Value of index 4: 29
+Mid value larger than 15, checking from left index of 0 to 3
+Checking Mid index of 0 + 3 = 1
+---
+Mid Value of index 1: 13
+Mid value smaller than 15, checking from left index of 2 to 3
+Checking Mid index of 2 + 3 = 2
+---
+Mid Value of index 2: 15
+"""
+
